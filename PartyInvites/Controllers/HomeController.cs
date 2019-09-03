22a778f5a18e77c7aa48040b9c4b56610ca10758
MZnNi <MZnNi@DESKTOP-R672079>
2019-09-03 23:09:23 +0300

    1

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -17,11 +17,32 @@ namespace PartyInvites.Controllers
             return View("MyView");
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
         }
 
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
         //public IActionResult Index()
         //{
         //    return View();
--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required (ErrorMessage ="Пожалуйста, введите свое имя")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Пожалуйста введите свой Email")]
+        [RegularExpression (".+\\@.+\\..+", ErrorMessage ="Пожалуйста введите валидный Email")]
         public string Email { get; set; }
+        [Required(ErrorMessage ="Пожалуйста, введите свой номер телефона")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, укажите примети ли участие")]
         public bool? WillAttend { get; set; }
 
     }
