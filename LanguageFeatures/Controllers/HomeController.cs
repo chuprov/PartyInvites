1afb1d4ecadc5d20f0b700f89e786bba361b8dd4
MZnNi <MZnNi@DESKTOP-R672079>
2019-09-08 21:26:22 +0300

    4

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
@@ -10,7 +11,23 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            return View(new string[] {"C#","Language","Features"});
+            //return View(new string[] {"C#","Language","Features"});
+
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name??"<No Name>";
+                decimal? price = p?.Price??0;
+                string category = p?.Category??"<None>";
+                string relatedName = p?.Related?.Name??"<None>";
+                string instock = p?.InStock.ToString()??"<None ";
+
+                results.Add(string.Format("Name: {0}, Category:{1}, Price: {2}, Related: {3}, InStock {4}", name, category, price, relatedName,instock));
+            }
+            
+            return View(results);
         }
     }
 }
+ 
